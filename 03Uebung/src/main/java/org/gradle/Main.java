ad9532e184dbd27f33c4d8d3934e7db7cf042969
bananatreedad <jnt.meier@gmail.com>
2015-10-16 17:22:21 +0200

    delte test methods

--- 03Uebung/src/main/java/org/gradle/Main.java
+++ 03Uebung/src/main/java/org/gradle/Main.java
@@ -4,33 +4,5 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		printHello("Johnny");
-		printHello("Chris");
-		
-		System.out.println(addTwoNumbers(5, 7));
-	}
-
-	/**
-	 * Prints "Hello" + the name. e.g. "Hello Johnny!"
-	 * 
-	 * @param name
-	 * @author gotgoxed 
-	 */
-	public static void printHello(String name) {
-
-		System.out.println("Hello " + name);
-	}
-
-	/**
-	 * Adds the two numbers
-	 * 
-	 * @return addition of i & b
-	 * @param i
-	 * @param b
-	 */
-	public static int addTwoNumbers(int i, int b) {
-
-		int result = i + b;
-		return result;
 	}
 }
