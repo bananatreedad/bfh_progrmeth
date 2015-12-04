c26a264c609b168f2fb453d1e4e83c45d1582b7c
bananatreedad <jnt.meier@gmail.com>
2015-12-04 07:10:49 +0100

    add menu to ControllerGUI

--- StopWatchEx/src/application/mvc/ControllerGUI.java
+++ StopWatchEx/src/application/mvc/ControllerGUI.java
@@ -3,42 +3,71 @@ package application.mvc;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class ControllerGUI extends Stage {
-	
+
 	@SuppressWarnings("unused")
 	private final Controller controller;
 
 	public ControllerGUI(Controller controller) {
 		this.controller = controller;
-		
+
+		BorderPane pane = new BorderPane();
+
 		HBox box = new HBox();
+		pane.setCenter(box);
+
 		box.setSpacing(10);
 		box.setPadding(new Insets(20));
-		
+
 		Button startButton = new Button("start");
 		startButton.setPrefWidth(100);
 		Button stopButton = new Button("stop");
 		stopButton.setPrefWidth(100);
 		Button resetButton = new Button("reset");
 		resetButton.setPrefWidth(100);
-		
-		startButton.setOnAction(event -> controller.start()); 
-		stopButton.setOnAction(event -> controller.stop()); 
-		resetButton.setOnAction(event -> controller.reset()); 
+
+		MenuBar menuBar = new MenuBar();
+		Menu appMenu = new Menu("Application");
+		MenuItem closeItem = new MenuItem("Close");
+		appMenu.getItems().add(closeItem);
+		Menu stopwatchMenu = new Menu("Stopwatch");
+		MenuItem newStopwatchItem = new MenuItem("New Stopwatch");
+		stopwatchMenu.getItems().add(newStopwatchItem);
+
+		stopwatchMenu.setOnAction(event -> {
+
+			TimerTeacher newTimer = new TimerTeacher(100);
+			Controller newController = new Controller(newTimer);
+
+			new Stopwatch(newTimer, newController, 0, 0);
+
+		});
+
+		closeItem.setOnAction(event -> System.exit(0));
+
+		menuBar.getMenus().addAll(appMenu, stopwatchMenu);
+		pane.setTop(menuBar);
+
+		startButton.setOnAction(event -> controller.start());
+		stopButton.setOnAction(event -> controller.stop());
+		resetButton.setOnAction(event -> controller.reset());
 
 		box.getChildren().addAll(startButton, stopButton, resetButton);
-		
-		Scene scene = new Scene(box);
-		
+
+		Scene scene = new Scene(pane);
+
 		this.setScene(scene);
 		this.setTitle("ControllerGUI");
 		this.setX(780);
 		this.setY(350);
 		this.show();
 	}
-	
-	
+
 }
